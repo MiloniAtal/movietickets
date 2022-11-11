92809177b8ab5157564641a28d16cd8b0f95c662
Miloni Atal <miloniatal@gmail.com>
2022-11-11 17:13:51 -0500

    now see

--- server.py
+++ server.py
@@ -190,13 +190,13 @@ def venues():
 
 @app.route('/venue/<vid>')
 def venue(vid):
-  cursor = g.conn.execute("SELECT T.date, M.name, T.starttime FROM Movie NATURAL JOIN Shows NATURAL JOIN Timings WHERE vid={vid}".format(vid=vid))
+  # cursor = g.conn.execute("SELECT T.date, M.name, T.starttime FROM Movie NATURAL JOIN Shows NATURAL JOIN Timings WHERE vid={vid}".format(vid=vid))
   # venue_shows = []
   # for result in cursor:
   #   row = [result["date"], result["name"], result["startime"]]
   #   venue_shows.append(row)
   # print(venue_shows)
-  cursor.close()
+  # cursor.close()
   # context = dict(data = venue_shows)
   # return render_template("venue.html", **context)
 
