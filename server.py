117ea21e0cb69550bcd0e06099095f3007248f7c
Miloni Atal <miloniatal@gmail.com>
2022-11-13 14:21:27 -0500

    try

--- server.py
+++ server.py
@@ -191,7 +191,7 @@ def venues():
 
 @app.route('/venue_search/<vid>')
 def venue(vid):
-  cursor = g.conn.execute("SELECT T.date, M.name, T.starttime, theatrename FROM Movie M NATURAL JOIN Shows S NATURAL JOIN Timing T WHERE vid={vid} ORDER BY T.date, M.name, T.starttime ASC".format(vid=vid))
+  cursor = g.conn.execute("SELECT T.date, M.name, T.starttime, theatrename FROM Movie M NATURAL JOIN Shows S NATURAL JOIN Timing T WHERE vid={vid} ORDER BY T.date, M.name, theatrename, T.starttime ASC".format(vid=vid))
   venue_shows = []
   for result in cursor:
     row = [result["date"], result["name"], result["starttime"], result["theatrename"]]
